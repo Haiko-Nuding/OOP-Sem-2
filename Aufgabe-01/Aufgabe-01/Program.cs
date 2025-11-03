bdd2e6b4e5709a7c4c59b05a3afe1a5cb59ae778
Haiko <haikonudingmorales@yahoo.com>
2025-11-03 19:07:04 +0100

    [new] aufgabe-01: added solution for aufgabe-01. Which is a small c# console app.

---
+++ Aufgabe-01/Aufgabe-01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Aufgabe-01: Variablen
+// Description: Notieren Sie drei gültige Deklarationen für primitive Datentypen
+
+// 1. Einfache Variablendeklaration
+int age;
+
+// 2. Mehrere Variablen gleichen Typs deklarieren
+int pageNumber, total;
+
+// 3. Deklaration mit gleichzeitiger Initialisierung
+int counter = 0;
