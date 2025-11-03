d246bf83ed80cf48eb6d550fc7adb543ae261394
Haiko <haikonudingmorales@yahoo.com>
2025-11-03 22:54:42 +0100

    [change] aufgabe-03: new programm for writting current time on the terminal! Tricky, had to use Thread sleep and learned '\r'.

---
+++ Aufgabe-03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aufgabe_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string time = DateTime.Now.ToString("HH:mm:ss");
+                Console.Write($"\rAktuelle Uhrzeit: " + time);
+                Thread.Sleep(1000); // 1 Sekunde warten
+            }
+        }
+    }
+}
