30ff58d43c32eecd1cbee8cb2589d59323d3f433
Haiko <haikonudingmorales@yahoo.com>
2025-11-03 23:08:21 +0100

    [change] aufgabe-03: update programm, missed break condition in while loop. should have added a cntrl + x as the break condition.

--- Aufgabe-03/Program.cs
+++ Aufgabe-03/Program.cs
@@ -13,6 +13,19 @@ namespace Aufgabe_03
         {
             while (true)
             {
+                // Wenn eine Taste gedrückt wird:
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true); // true = nicht anzeigen   
+
+                    // Prüfen ob CTRL + X gedrückt wurde
+                    if (key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key == ConsoleKey.X)
+                    {
+                        Console.WriteLine("\nProgramm wird beendet...");
+                        break; // Endlosschleife verlassen
+                    }
+                }
+
                 string time = DateTime.Now.ToString("HH:mm:ss");
                 Console.Write($"\rAktuelle Uhrzeit: " + time);
                 Thread.Sleep(1000); // 1 Sekunde warten
