96b2ef4ab0cefad844ebebcc6473ac2eb3adc016
Haiko <haikonudingmorales@yahoo.com>
2025-11-05 00:49:09 +0100

    [new] aufgabe-11: added new project containing class 'Bankkonto'.

---
+++ Aufgabe-11/Bankkonto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_11
+{
+    internal class Bankkonto
+    {
+        private decimal _saldo;
+
+        public Bankkonto(decimal startGuthaben)
+        {
+            _saldo = startGuthaben;
+            Console.WriteLine($"Konto eröffnet mit Startguthaben: {startGuthaben} CHF");
+        }
+
+        public void Einzahlen(decimal betrag)
+        {
+            if (betrag > 0)
+            {
+                _saldo += betrag;
+                Console.WriteLine($"{betrag} CHF wurde eingezahlt.");
+            }
+            else 
+            {
+                Console.WriteLine($"Einzahlung fehlgeschlagen. Bitte geben Sie einen positiven betrag ein.");
+            }
+        }
+
+        public void Auszahlen(decimal betrag)
+        {
+            if (betrag >= 0)
+            {
+                if (betrag <= _saldo)
+                {
+                    _saldo -= betrag;
+                    Console.WriteLine($"{betrag} CHF wurde ausgezahlt.");
+                }
+                else
+                {
+                    Console.WriteLine($"Nicht genügend Saldo. Bitte einen anderen Betrag eingeben.");
+                }
+                
+            }
+            else 
+            {
+                Console.WriteLine($"Auszahlung fehlgeschlagen. Bitte geben Sie einen positiven betrag ein.");
+            }
+        }
+
+        public void ZinsGutschrift(decimal zinssatzProzent)
+        {
+            decimal zins = _saldo * (zinssatzProzent / 100m);
+            Console.WriteLine($"Zinsen ({zinssatzProzent}%): {zins} CHF wurden gutgeschrieben.");
+            _saldo += zins;
+        }
+
+        public void SaldoAnzeigen() 
+        {
+            Console.WriteLine($"Ihr Saldo beträgt: {_saldo}");
+        }
+    }
+}
---
+++ Aufgabe-11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bankkonto konto = new Bankkonto(1000);
+            konto.Einzahlen(200);
+            konto.Auszahlen(150);
+            konto.ZinsGutschrift(1.5m);
+            konto.SaldoAnzeigen();
+
+            // Test 01: This should fail
+            konto.Auszahlen(90000);
+            konto.SaldoAnzeigen();
+
+            // Test 02: This should work
+            konto.Auszahlen(1000);
+            konto.SaldoAnzeigen();
+
+            // Test 03: Testing negative values for 'Einzahlen' function
+            konto.Einzahlen(-200);
+            konto.SaldoAnzeigen();
+
+            // Test 04: Testing negative values for 'Auszahlen' function
+            konto.Auszahlen(-1000);
+            konto.SaldoAnzeigen();
+        }
+    }
+}
