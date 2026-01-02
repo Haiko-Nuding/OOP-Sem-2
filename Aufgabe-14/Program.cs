76f078639da987f8b24ff10093a915fbd582c303
Haiko <haikonudingmorales@yahoo.com>
2026-01-02 23:16:54 +0100

    [new] aufgabe-14: added new project ticket counter learning static class methods.

---
+++ Aufgabe-14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erstellen von Instanzen
+            Ticket t1 = new Ticket("Konzert", 45.00);
+            Ticket t2 = new Ticket("Kino", 15.00);
+            Ticket t3 = new Ticket("Fussballspiel", 30.00);
+
+            // Auslesen über die statische Methode
+            Console.WriteLine($"Anzahl verkaufter Tickets: {Ticket.GetTicketCount()}");
+        }
+    }
+}
---
+++ Aufgabe-14/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_14
+{
+    internal class Ticket
+    {
+        private static int _ticketCount = 0;
+
+        public string EventName { get; set; }
+        public double Price { get; set; }
+
+        public Ticket(string eventName, double price)
+        {
+            EventName = eventName;
+            Price = price;
+
+            _ticketCount++;
+        }
+
+        public static int GetTicketCount()
+        {
+            return _ticketCount;
+        }
+    }
+}
