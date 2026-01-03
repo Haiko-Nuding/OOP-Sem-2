358f5bf7afff0afc2e383fe4cea618fb40744cba
Haiko <haikonudingmorales@yahoo.com>
2026-01-03 19:47:23 +0100

    [new] aufgabe-17: new task Klimaananlage using enums and refreshing properties.

---
+++ Aufgabe-17/Klimaanlage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Modus
+{
+    Heizen,
+    Off,
+    Kuehlen
+}
+
+namespace Aufgabe_17
+{
+    internal class Klimaanlage
+    {
+        private double zielTemperatur;
+
+        // Aktuelle Temperatur
+        public double AktuelleTemperatur { get; set; }
+
+        // ZielTemperatur mit Logik für Modus
+        public double ZielTemperatur
+        {
+            get => zielTemperatur;
+            set
+            {
+                zielTemperatur = value;
+                // Modus automatisch setzen
+                if (AktuelleTemperatur < zielTemperatur)
+                {
+                    Modus = Modus.Heizen;
+                }
+                else if (AktuelleTemperatur > zielTemperatur)
+                {
+                    Modus = Modus.Kuehlen;
+                }
+                else
+                {
+                    Modus = Modus.Off;
+                }
+            }
+        }
+
+        // Aktueller Modus
+        public Modus Modus { get; private set; }
+
+        // 3️⃣ Methode: Status anzeigen
+        public void StatusAnzeigen()
+        {
+            Console.WriteLine("Aktuelle Temperatur: " + AktuelleTemperatur + "°C");
+            Console.WriteLine("Zieltemperatur: " + ZielTemperatur + "°C");
+            Console.WriteLine("Modus: " + Modus);
+            Console.WriteLine(); // Leerzeile für Übersicht
+        }
+    }
+}
---
+++ Aufgabe-17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Klimaanlage erzeugen
+            Klimaanlage klima = new Klimaanlage();
+
+            // Aktuelle Temperatur setzen
+            klima.AktuelleTemperatur = 20.0;
+
+            // Zieltemperatur setzen → Modus wird automatisch angepasst
+            klima.ZielTemperatur = 22.0;
+            klima.StatusAnzeigen();  // Heizen
+
+            klima.ZielTemperatur = 20.0;
+            klima.StatusAnzeigen();  // Off
+
+            klima.ZielTemperatur = 18.0;
+            klima.StatusAnzeigen();  // Kuehlen
+        }
+    }
+}
